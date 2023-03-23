35b8003fdc4767cfa18282dfa51becec9ccab8a1
Orlando Flores Silva <orlandofury@gmail.com>
2023-03-22 21:55:28 -0600

    Seccion 2 After automapper

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -13,42 +13,19 @@ namespace StudentAdminPortal.API.Controllers
     public class StudentsController : Controller
     {
         private readonly IStudentRepository studentRepository;
-        
+        private readonly IMapper mapper;
 
-        public StudentsController(IStudentRepository studentRepository)
+        public StudentsController(IStudentRepository studentRepository,IMapper mapper)
         {
             this.studentRepository = studentRepository;
-        
+            this.mapper = mapper;
         }
         [HttpGet]
         [Route("[controller]")]
         public IActionResult GetAllStudents()
         {
-            var students = studentRepository.GetStudents()
-                           .Select(student => new Student
-                           {
-                               Id = student.Id,
-                               FirstName = student.FirstName,
-                               LastName = student.LastName,
-                               DateOfBirth = student.DateOfBirth,
-                               Email = student.Email,
-                               Mobile = student.Mobile,
-                               ProfileImageUrl = student.ProfileImageUrl,
-                               GenderId = student.GenderId,
-                               Address = new Address()
-                               {
-                                   Id = student.Address.Id,
-                                   PhysicalAddress = student.Address.PhysicalAddress,
-                                   PostalAddress = student.Address.PostalAddress
-                               },
-                               Gender = new Gender()
-                               {
-                                   Id = student.Gender.Id,
-                                   Description = student.Gender.Description
-                               }
-                           });
-            var domainModelStudent = students.ToList();
-            return Ok(domainModelStudent);
+            var students = studentRepository.GetStudents();
+            return Ok(mapper.Map<List<Student>>(students));
         }
     }
 }
